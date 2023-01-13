e19127e06f8bbd9dca2f80e47e39ba0387df9842
Esra-Turk <esra.turk@bil.omu.edu.tr>
2023-01-13 17:49:58 +0300

    yardimlar routerı güncellendi

--- routes/yardimlar.js
+++ routes/yardimlar.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const { getYardimlar } = require('../actions/yardimlar/get');
+const { selectAllColumns } = require('../actions/get');
+const { connection } = require('../db/connection');
 const router = express.Router();
 
-router.get('/', getYardimlar);
+router.get('/', (_, res) => {
+    selectAllColumns('Yardim', res);
+});
+router.get('/:id', (req, res) => {
+    connection.query('SELECT * FROM Yardim WHERE yNo = ?', [req.params.id], (err, rows) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
 
 module.exports = router;
